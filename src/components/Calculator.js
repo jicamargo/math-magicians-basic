cf49c9ee3177798e78485529265aa509e18fa631
jic <ji_camargo@yahoo.com>
2023-06-07 17:52:20 -0500

    refactor component Button, delete Operator and fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,86 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../calculator.css';
 import Button from './Button';
-import Operator from './Operator';
 import Display from './Display';
 import calculate from './logic/calculate';
 
 function Calculator() {
-  const handleClick = (buttonName) => {
-    const result = calculate({ total: null, next: null, operation: null }, buttonName);
-    console.log(result);
-  };
+  const [calculation, setCalculation] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const [display, setDisplay] = useState('0');
 
+  const handleClick = (buttonName) => {
+    const result = calculate(calculation, buttonName);
+    setCalculation(result);
+    setDisplay(result.next || result.total || '0');
+  };
 
   return (
     <div className="calculator">
       <div className="row display-container">
-        <Display />
+        <Display result={display} />
       </div>
       <div className="row">
-        <Button type="reset" text="AC"
+        <Button
+          type="reset"
+          text="AC"
           onClick={() => handleClick('AC')}
         />
-        <Button type="plus-minus" text="+/-" 
+        <Button
+          type="plus-minus"
+          text="+/-"
           onClick={() => handleClick('+/-')}
         />
-        <Button type="operator" text="%" 
+        <Button
+          type="operator"
+          text="%"
           onClick={() => handleClick('%')}
         />
-        <Operator text="/" className="yellow" 
-          onClick={() => handleClick('/')}
+        <Button
+          type="operator"
+          text="÷"
+          className="yellow"
+          onClick={() => handleClick('÷')}
         />
       </div>
       <div className="row">
-        <Button type="number" text="7" 
+        <Button
+          type="number"
+          text="7"
           onClick={() => handleClick('7')}
         />
-        <Button type="number" text="8" 
+        <Button
+          type="number"
+          text="8"
           onClick={() => handleClick('8')}
         />
-        <Button type="number" text="9" 
+        <Button
+          type="number"
+          text="9"
           onClick={() => handleClick('9')}
         />
-        <Operator text="X" className="yellow" 
-          onClick={() => handleClick('X')}
+        <Button
+          type="operator"
+          text="x"
+          className="yellow"
+          onClick={() => handleClick('x')}
         />
       </div>
       <div className="row">
-        <Button type="number" text="4"
+        <Button
+          type="number"
+          text="4"
           onClick={() => handleClick('4')}
         />
-        <Button type="number" text="5"
+        <Button
+          type="number"
+          text="5"
           onClick={() => handleClick('5')}
         />
-        <Button type="number" text="6"
+        <Button
+          type="number"
+          text="6"
           onClick={() => handleClick('6')}
         />
-        <Operator text="-" className="yellow"
+        <Button
+          type="operator"
+          text="-"
+          className="yellow"
           onClick={() => handleClick('-')}
         />
       </div>
       <div className="row">
-        <Button type="number" text="2" 
-          onClick={() => handleClick('2')}
-        />        
-        <Button type="number" text="3" 
-          onClick={() => handleClick('3')}
-        />
-        <Button type="number" text="1" 
+        <Button
+          type="number"
+          text="1"
           onClick={() => handleClick('1')}
         />
-        <Operator text="+" className="yellow" 
+        <Button
+          type="number"
+          text="2"
+          onClick={() => handleClick('2')}
+        />
+        <Button
+          type="number"
+          text="3"
+          onClick={() => handleClick('3')}
+        />
+        <Button
+          type="operator"
+          text="+"
+          className="yellow"
           onClick={() => handleClick('+')}
         />
       </div>
       <div className="row">
-        <Button type="number wide" text="0" 
+        <Button
+          type="number wide"
+          text="0"
           onClick={() => handleClick('0')}
         />
-        <Button type="number" text="." 
+        <Button
+          type="number"
+          text="."
           onClick={() => handleClick('.')}
         />
-        <Operator type="result" text="=" className="yellow"
+        <Button
+          type="operator"
+          text="="
+          className="yellow"
           onClick={() => handleClick('=')}
         />
       </div>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Display() {
-  return <div className="display">0</div>;
+function Display({ result }) {
+  // result = result || "0";
+  return (
+    <div className="display">
+      {result}
+    </div>
+  );
 }
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
+};
 
 export default Display;
